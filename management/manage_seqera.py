a99a97c1ac43ff8a9f870122b19e5c489c0f5273
mgeaghan <michael.p.geaghan@gmail.com>
2026-03-19 16:07:20 +1100

    class-ify and improve printing

--- management/manage_seqera.py
+++ management/manage_seqera.py
@@ -19,9 +19,7 @@ def parse_args():
     list_orgs = list_subparsers.add_parser('orgs', help='List available organisations.')
     list_workspaces = list_subparsers.add_parser('workspaces', help='List available workspaces.')
     list_workflows = list_subparsers.add_parser('workflows', help='List available workflows.')
-    list_workflows.add_argument('-w', '--workspace', help='Workspace ID', type=str, default='')
     list_runs = list_subparsers.add_parser('runs', help='List all runs in the workspace.')
-    list_launches = list_subparsers.add_parser('launches', help='List all launches in the workspace.')
 
     args = parser.parse_args()
 
@@ -31,185 +29,331 @@ def parse_args():
     return args
 
 
-class SeqeraApi:
+class SeqeraApiBase:
 
-    def __init__(self, token_path, org_id_or_name=None, workspace_id_or_name=None, workflow=None):
+    def __init__(self, token_path: str, endpoint: str) -> None:
         self.base_url = 'https://seqera.services.biocommons.org.au/api'
         self.token = self._get_token(token_path)
         self.auth_header = { 'Authorization': f'Bearer {self.token}' }
-        self.all_orgs = self._get_all_orgs()
-        self.set_org(org_id_or_name=org_id_or_name)
-        self.all_workspaces = self._get_all_workspaces()
-        self.set_workspace(workspace_id_or_name=workspace_id_or_name)
-        self.all_workflows = self._get_all_workflows()
-        self.all_runs = self._get_all_runs()
-        self.all_launches = self._get_all_launches()
+        self.url = self._get_api_url(endpoint)
+        self.json = self._get_response(self.url, self.auth_header)
 
-    def _get_endpoint(self, suffix):
-        base_url = str(self.base_url)
-        if base_url.endswith('/'):
-            base_url = re.sub(r'/$', '', base_url)
-        clean_suffix = str(suffix)
-        if not clean_suffix.startswith('/'):
-            clean_suffix = f'/{clean_suffix}'
-        return f'{self.base_url}{suffix}'
-
-    def set_org(self, org_id_or_name):
-        self.org_id_or_name = org_id_or_name
-        self.org = self._get_orgid()
-
-    def set_workspace(self, workspace_id_or_name):
-        self.workspace_id_or_name = workspace_id_or_name
-        self.workspace = self._get_workspace_id()
-
-    def _get_token(self, token_path):
+    def _get_token(self, token_path: str) -> str:
         with open(token_path, 'r') as f:
             token = f.readline().strip()
         return token
 
-    def _get_all_orgs(self):
-        endpoint = self._get_endpoint('/orgs')
-        r = requests.get(endpoint, headers=self.auth_header)
-        if r.status_code != 200:
-            raise ValueError('Error in fetching organisation list.')
-        return r.json()
+    def _get_api_url(self, endpoint: str) -> str:
+        base_url = str(self.base_url)
+        if base_url.endswith('/'):
+            base_url = re.sub(r'/$', '', base_url)
+        clean_endpoint = str(endpoint)
+        if not clean_endpoint.startswith('/'):
+            clean_endpoint = f'/{clean_endpoint}'
+        return f'{self.base_url}{clean_endpoint}'
 
-    def _get_orgid(self):
-        if not self.all_orgs or not self.org_id_or_name:
+    def _get_response(self, url, auth_header) -> None | dict | list:
+        if not url or not auth_header:
             return None
-        for org in self.all_orgs['organizations']:
-            if self.org_id_or_name == org['orgId']:
-                return self.org_id_or_name
-            elif self.org_id_or_name in [org['name'], org['fullName']]:
-                return org['orgId']
-        return None
-
-    def _get_all_workspaces(self):
-        if not self.org:
-            return {}
-        endpoint = self._get_endpoint(f'/orgs/{self.org}/workspaces')
-        r = requests.get(endpoint, headers=self.auth_header)
+        r = requests.get(url=url, headers=auth_header)
         if r.status_code != 200:
-            raise ValueError('Error in fetching workspace list.')
+            raise ValueError('Error in API request.')
         return r.json()
 
-    def _get_workspace_id(self):
-        if not self.all_workspaces or not self.workspace_id_or_name:
-            return None
-        for workspace in self.all_workspaces['workspaces']:
-            if self.workspace_id_or_name == workspace['id']:
-                return self.workspace_id_or_name
-            elif self.workspace_id_or_name in [workspace['name'], workspace['fullName']]:
-                return workspace['id']
-        return None
 
-    def _get_all_workflows(self):
-        endpoint = self._get_endpoint('/pipelines')
-        if self.workspace:
-            endpoint = self._get_endpoint(f'/pipelines?workspaceId={self.workspace}')
-        r = requests.get(endpoint, headers=self.auth_header)
-        if r.status_code != 200:
-            raise ValueError('Error in fetching workflow list.')
-        return r.json()
+class SeqeraOrg:
+
+    fields = ['Name', 'Full Name', 'Org ID']
+
+    def __init__(self, id: str, name: str, full_name: str) -> None:
+        self.id = id
+        self.name = name
+        self.full_name = full_name
     
-    def _get_all_runs(self):
-        endpoint = self._get_endpoint('/workflow')
-        if self.workspace:
-            endpoint = self._get_endpoint(f'/workflow?workspaceId={self.workspace}')
-        r = requests.get(endpoint, headers=self.auth_header)
-        if r.status_code != 200:
-            raise ValueError('Error in fetching workflow list.')
-        return r.json()
+    def as_list(self) -> list:
+        return [self.name, self.full_name, self.id]
 
-    def _get_all_launches(self):
-        if not self.all_runs:
-            return {}
-        launches = {}
-        for run in self.all_runs['workflows']:
-            run_id = run['workflow']['id']
-            endpoint = self._get_endpoint(f'/workflow/{run_id}/launch')
-            if self.workspace:
-                endpoint = f'{endpoint}?workspaceId={self.workspace}'
-            r = requests.get(endpoint, headers=self.auth_header)
-            if r.status_code != 200:
-                raise ValueError('Error in fetching workflow list.')
-            launches[run_id] = r.json()
-        return launches
 
-    def _list_orgs(self):
-        orgs = []
-        if not self.all_orgs:
-            return orgs
-        for org in self.all_orgs['organizations']:
-            orgs.append((org['orgId'], org['name']))
+class SeqeraOrgs(SeqeraApiBase):
+
+    def __init__(self, token_path: str) -> None:
+        endpoint = '/orgs'
+        super().__init__(token_path, endpoint)
+        self.orgs = self._parse_orgs()
+        self.id_lookup = self._get_org_id_lookup()
+        self.active_org_id = None
+        self.active_org = None
+
+    def _parse_orgs(self) -> dict:
+        assert self.json, 'Error: SeqeraOrgs object has not been properly initialised.'
+        orgs = {}
+        for org in self.json['organizations']:
+            id = str(org['orgId'])
+            name = str(org['name'])
+            full_name = str(org['fullName'])
+            orgs[id] = SeqeraOrg(id, name, full_name)
         return orgs
-    
-    def print_orgs(self):
-        for id, name in self._list_orgs():
-            print(f'{name}:\t{id}')
 
-    def _list_workspaces(self):
-        workspaces = []
-        if not self.all_workspaces:
-            return workspaces
-        for workspace in self.all_workspaces['workspaces']:
-            workspaces.append((workspace['id'], workspace['name']))
+    def _get_org_id_lookup(self) -> dict:
+        lookup = {}
+        for org in self.orgs.values():
+            id = org.id
+            name = org.name
+            full_name = org.full_name
+            lookup[id] = id
+            lookup[name] = id
+            lookup[full_name] = id
+        return lookup
+
+    def set_org(self, org_id_or_name: str) -> None:
+        self.active_org_id = self.id_lookup.get(org_id_or_name, None)
+        self.active_org = self.orgs[self.active_org_id]
+
+    def as_list(self) -> list:
+        return [org.as_list() for org in self.orgs.values()]
+
+
+class SeqeraWorkspace:
+
+    fields = ['Name', 'Workspace ID']
+
+    def __init__(self, id: str, name: str) -> None:
+        self.id = id
+        self.name = name
+    
+    def as_list(self) -> list:
+        return [self.name, self.id]
+
+
+class SeqeraWorkspaces(SeqeraApiBase):
+
+    def __init__(self, token_path: str, org: SeqeraOrg) -> None:
+        endpoint = f'/orgs/{org.id}/workspaces'
+        super().__init__(token_path, endpoint)
+        self.workspaces = self._parse_workspaces()
+        self.id_lookup = self._get_workspaces_id_lookup()
+        self.active_workspace_id = None
+        self.active_workspace = None
+
+    def _parse_workspaces(self) -> dict:
+        assert self.json, 'Error: SeqeraWorkspaces object has not been properly initialised.'
+        workspaces = {}
+        for workspace in self.json['workspaces']:
+            id = str(workspace['id'])
+            name = str(workspace['name'])
+            workspaces[id] = SeqeraWorkspace(id, name)
         return workspaces
 
-    def print_workspaces(self):
-        for id, name in self._list_workspaces():
-            print(f'{name}:\t{id}')
+    def _get_workspaces_id_lookup(self) -> dict:
+        lookup = {}
+        for workspace in self.workspaces.values():
+            id = workspace.id
+            name = workspace.name
+            lookup[id] = id
+            lookup[name] = id
+        return lookup
 
-    def _list_workflows(self):
-        workflows = []
-        if not self.all_workflows:
-            return workflows
-        for workflow in self.all_workflows['pipelines']:
-            workflows.append((
-                workflow['pipelineId'],
-                workflow['repository'],
-                workflow['name'],
-            ))
+    def set_workspace(self, workspace_id_or_name: str) -> None:
+        self.active_workspace_id = self.id_lookup.get(workspace_id_or_name, None)
+        self.active_workspace = self.workspaces[self.active_workspace_id]
+
+    def as_list(self) -> list:
+        return [workspace.as_list() for workspace in self.workspaces.values()]
+
+
+class SeqeraWorkflow:
+
+    fields = ['Name', 'Repository', 'Workflow ID', 'Deleted']
+
+    def __init__(self, id: str, name: str, repo: str, deleted: bool = False) -> None:
+        self.id = id
+        self.name = name
+        self.repo = repo
+        self.deleted = deleted
+
+    def as_list(self) -> list:
+        return [self.name, self.repo, self.id, self.deleted]
+
+
+class SeqeraWorkflows(SeqeraApiBase):
+
+    def __init__(self, token_path: str, org: SeqeraOrg, workspace: SeqeraWorkspace | None = None) -> None:
+        endpoint = f'/pipelines'
+        if workspace:
+            endpoint = f'{endpoint}?workspaceId={workspace.id}'
+        super().__init__(token_path, endpoint)
+        self.workflows = self._parse_workflows()
+
+    def _parse_workflows(self) -> dict:
+        assert self.json, 'Error: SeqeraWorkflows object has not been properly initialised.'
+        workflows = {}
+        for workflow in self.json['pipelines']:
+            id = str(workflow['pipelineId'])
+            name = str(workflow['name'])
+            repo = str(workflow['repository'])
+            workflows[id] = SeqeraWorkflow(id, name, repo)
         return workflows
 
-    def print_workflows(self):
-        for id, repo, name in self._list_workflows():
-            print(f'{name}:\t{repo}\t{id}')
+    def as_list(self) -> list:
+        return [workflow.as_list() for workflow in self.workflows.values()]
 
-    def _list_runs(self):
-        runs = []
-        if not self.all_runs:
-            return runs
-        for run in self.all_runs['workflows']:
-            workflow = run['workflow']
-            runs.append((
-                workflow['id'],
-                workflow['launchId'],
-                workflow['projectName'],
-                workflow['runName'],
-            ))
-        return runs
 
-    def print_runs(self):
-        for id, launch_id, project_name, run_name in self._list_runs():
-            print(f'{run_name}:\t{project_name}\t{id}\t{launch_id}')
+class SeqeraRun:
 
-    def _list_launches(self):
-        launches = []
-        if not self.all_launches:
-            return launches
-        for run_id, launch in self.all_launches.items():
-            launch_info = launch['launch']
-            launches.append((
-                launch_info['id'],
-                run_id,
-                launch_info['pipelineId']
-            ))
+    fields = ['Run ID', 'Run Name', 'Launch ID', *SeqeraWorkflow.fields]
+
+    def __init__(self, id: str, name: str, launch_id: str, workflow: SeqeraWorkflow):
+        self.id = id
+        self.name = name
+        self.launch_id = launch_id
+        self.workflow = workflow
+
+    def as_list(self) -> list:
+        workflow_fields = ['NA'] * len(SeqeraWorkflow.fields)
+        if self.workflow:
+            workflow_fields = self.workflow.as_list()
+        return [self.id, self.name, self.launch_id, *workflow_fields]
+
+
+class SeqeraLaunch:
+
+    fields = ['Launch ID', 'Run ID', 'Workflow ID']
+
+    def __init__(self, id: str, run_id: str, workflow_id: str | None):
+        self.id = id
+        self.run_id = run_id
+        self.workflow_id = workflow_id
+
+    def as_list(self) -> list:
+        return [self.id, self.run_id, self.workflow_id]
+
+
+class SeqeraRuns(SeqeraApiBase):
+
+    def __init__(self, token_path: str, org: SeqeraOrg, workflows: SeqeraWorkflows, workspace: SeqeraWorkspace | None = None) -> None:
+        endpoint = '/workflow'
+        if workspace:
+            endpoint = f'{endpoint}?workspaceId={workspace.id}'
+        super().__init__(token_path, endpoint)
+        self.workflows = workflows
+        self.deleted_workflow_info = {}
+        self.launches_json = self._get_launches(workspace)
+        self.launches = self._parse_launches()
+        self.runs = self._parse_runs(workspace)
+
+    def _get_launches(self, workspace: SeqeraWorkspace | None = None) -> dict:
+        assert self.json, 'Error: SeqeraRuns object has not been properly initialised.'
+        launches = {}
+        for run in self.json['workflows']:
+            run_id = run['workflow']['id']
+            launch_endpoint = f'/workflow/{run_id}/launch'
+            if workspace:
+                launch_endpoint = f'{launch_endpoint}?workspaceId={workspace.id}'
+            launch_url = self._get_api_url(launch_endpoint)
+            launch = self._get_response(launch_url, self.auth_header)
+            launches[run_id] = launch
         return launches
 
-    def print_launches(self):
-        for launch_id, run_id, pipeline_id in self._list_launches():
-            print(f'{run_id}:\t{launch_id}\t{pipeline_id}')
+    def _get_deleted_workflow_info(self, workflow_id: str, workspace: SeqeraWorkspace | None = None) -> SeqeraWorkflow:
+        if workflow_id not in self.deleted_workflow_info:
+            endpoint = f'/pipelines/{workflow_id}'
+            if workspace:
+                endpoint = f'{endpoint}?workspaceId={workspace.id}'
+            url = self._get_api_url(endpoint)
+            workflow_json = self._get_response(url, self.auth_header)
+            workflow_info = workflow_json['pipeline']
+            name = workflow_info['name']
+            repo = workflow_info['repository']
+            self.deleted_workflow_info[workflow_id] = SeqeraWorkflow(workflow_id, name, repo, deleted=True)
+        return self.deleted_workflow_info[workflow_id]
+
+    def _parse_launches(self) -> dict:
+        assert self.launches_json, 'Error: SeqeraRuns object has not been properly initialised.'
+        launches = {}
+        for run_id, launch in self.launches_json.items():
+            launch_info = launch['launch']
+            launch_id = str(launch_info['id'])
+            workflow_id = str(launch_info['pipelineId']) if launch_info['pipelineId'] else None
+            launches[launch_id] = SeqeraLaunch(launch_id, run_id, workflow_id)
+        return launches
+
+    def _parse_runs(self, workspace: SeqeraWorkspace | None = None) -> dict:
+        assert self.json and self.launches, 'Error: SeqeraRuns object has not been properly initialised.'
+        runs = {}
+        for run in self.json['workflows']:
+            workflow = run['workflow']
+            run_id = str(workflow['id'])
+            run_name = str(workflow['runName'])
+            launch_id = str(workflow['launchId'])
+            launch_info = self.launches[launch_id]
+            if not launch_info.workflow_id:
+                workflow_info = SeqeraWorkflow('NA', 'NA', 'NA', True)
+            else:
+                workflow_id = str(launch_info.workflow_id)
+                workflow_info = self.workflows.workflows.get(workflow_id, None)
+                if not workflow_info:
+                    workflow_info = self._get_deleted_workflow_info(workflow_id, workspace)
+            runs[run_id] = SeqeraRun(run_id, run_name, launch_id, workflow_info)
+        return runs
+
+    def as_list(self) -> list:
+        return [run.as_list() for run in self.runs.values()]
+
+
+class SeqeraApi:
+
+    def __init__(self, token_path: str, org_id_or_name: None | str = None, workspace_id_or_name: None | str = None):
+        self.orgs = SeqeraOrgs(token_path)
+        self.workspaces = None
+        self.workflows = None
+        self.runs = None
+        if org_id_or_name:
+            self.orgs.set_org(org_id_or_name)
+            self.workspaces = SeqeraWorkspaces(token_path, self.orgs.active_org)
+            if workspace_id_or_name:
+                self.workspaces.set_workspace(workspace_id_or_name)
+            self.workflows = SeqeraWorkflows(token_path, self.orgs.active_org, self.workspaces.active_workspace)
+            self.runs = SeqeraRuns(token_path, self.orgs.active_org, self.workflows, self.workspaces.active_workspace)
+
+    def _print(self, header_list, body_list):
+        header_len = len(header_list)
+        full_list = [header_list, *body_list]
+        col_widths = [0] * header_len
+        for row in full_list:
+            assert len(row) == header_len, 'Error: inconsistent row lengths found.'
+            for i, elem in enumerate(row):
+                if len(str(elem)) > col_widths[i]:
+                    col_widths[i] = len(str(elem))
+        sep_row = ['-' * w for w in col_widths]
+        full_list = [header_list, sep_row, *body_list]
+        for row in full_list:
+            formatted_row = [
+                str(elem) + ' ' * (col_widths[i] - len(str(elem)))
+                for i, elem in enumerate(row)
+            ]
+            formatted_row = ' | '.join(formatted_row)
+            formatted_row = re.sub(r'\s+$', '', formatted_row)
+            print(formatted_row)
+
+    def print_orgs(self):
+        header = SeqeraOrg.fields
+        body = self.orgs.as_list()
+        self._print(header, body)
+
+    def print_workspaces(self):
+        header = SeqeraWorkspace.fields
+        body = self.workspaces.as_list() if self.workspaces else []
+        self._print(header, body)
+
+    def print_workflows(self):
+        header = SeqeraWorkflow.fields
+        body = self.workflows.as_list() if self.workflows else []
+        self._print(header, body)
+
+    def print_runs(self):
+        header = SeqeraRun.fields
+        body = self.runs.as_list() if self.runs else []
+        self._print(header, body)
 
 
 def main(args):
@@ -226,8 +370,6 @@ def main(args):
             api.print_workflows()
         elif args.list_subcommand == 'runs':
             api.print_runs()
-        elif args.list_subcommand == 'launches':
-            api.print_launches()
 
 
 if __name__ == '__main__':
